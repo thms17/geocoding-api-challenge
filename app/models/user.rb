e00356d4094445a4ca8155b1697c7d346232eb8b
Thomas Etscheidt <etscheidtt@yahoo.com>
2019-04-29 20:58:16 +0200

    wrote create method and token generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
+require 'securerandom'
 class User < ApplicationRecord
+  before_create :set_auth_token
+
+  def create
+    @user = User.new
+    @user.save
+  end
+
+  private
+
+  def set_auth_token
+    return if auth_token.present?
+
+    self.auth_token = generate_auth_token
+  end
+
+  def generate_auth_token
+    SecureRandom.uuid.gsub(/\-/,'')
+  end
 end
