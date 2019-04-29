17c569976e7a36a832068a3fa62813a13ee4469e
Thomas Etscheidt <etscheidtt@yahoo.com>
2019-04-29 21:12:47 +0200

    added auth_token column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_29_185451) do
+ActiveRecord::Schema.define(version: 2019_04_29_191149) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
   end
 
 end
